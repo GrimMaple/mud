dc95cf830249145254b364f9f5bce044f4a1e9ad
Grim Maple <grimmaple95@gmail.com>
2020-03-01 13:02:31 +0300

    Add growing container

---
+++ source/mud/container/growing.d
@@ -0,0 +1,100 @@
+module mud.container.growing;
+
+import std.experimental.allocator.mallocator;
+
+@safe @nogc nothrow:
+
+/// A container of primitives of type `T` that automatically grows when necessary. 
+/// The `size` parameter is the growth size
+struct GrowingContainer(T, size_t size = 1000, Allocator = Mallocator)
+if(__traits(isArithmetic, T))
+{
+    /// Accesses element at index `index`
+    auto opIndex(size_t index)
+    in(index < pos)
+    {
+        return primitives[index];
+    }
+    ///
+    unittest
+    {
+        GrowingContainer!int container;
+        container.put(10);
+        assert(container[0] == 10);
+    }
+
+    /// Returns container's internal capacity
+    @property size_t capacity() { return sz; }
+    ///
+    unittest
+    {
+        GrowingContainer!(int, 10) container;
+        assert(container.capacity == 0); // Container allocates lazily
+        container.put(10);
+        assert(container.capacity == 10); // Allocates in chunks of `size`
+    }
+
+    /// Returns count of elements in this container
+    @property size_t count() { return pos; }
+    ///
+    unittest
+    {
+        GrowingContainer!int container;
+        assert(container.count == 0);
+        container.put(10);
+        assert(container.count == 1);
+    }
+
+    /// Assigning to is disabled
+    void opAssign(GrowingContainer!(T, size, Allocator) rhs) @disable;
+
+    /// Copying is disabled
+    this(GrowingContainer!(T, size, Allocator) rhs) @disable;
+
+    /// Put a new element `val` into the container
+    void put(T val) @trusted
+    {
+        if(pos >= sz)
+            grow();
+        primitives[pos] = val;
+        pos++;
+    }
+    ///
+    unittest
+    {
+        GrowingContainer!int container;
+        container.put(10);
+        assert(container[0] == 10);
+    }
+
+    ~this() @trusted
+    {
+        Allocator.instance.deallocate(primitives);
+    }
+
+private:
+    void grow() @trusted
+    {
+        void[] r = cast(void[]) primitives;
+        T[] newMem = cast(int[])Allocator.instance.allocate(T.sizeof * (sz + size));
+        newMem[0 .. sz] = primitives[0 .. sz];
+        Allocator.instance.deallocate(primitives);
+        primitives = newMem;
+        sz += size;
+    }
+    size_t pos = 0;
+    T[] primitives = null;
+    size_t sz = 0;
+}
+///
+@trusted unittest
+{
+    GrowingContainer!(int, 10) container;
+    assert(container.capacity == 0);
+    container.put(10);
+    assert(container.capacity == 10 && container.count == 1);
+    foreach(i; 0 .. 10)
+        container.put(i);
+    assert(container.capacity == 20 && container.count == 11);
+    assert(container[1] == 0 && container[0] == 10);
+}
